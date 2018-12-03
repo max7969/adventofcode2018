f351815d440d24f9e2e82b6b0f42280a5dcb02ad
Andre Maxime <maxime.andre@zenika.com>
2018-12-03 22:39:52 +0100

    day3:step1

---
+++ day3/day3_step1.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+let data = JSON.parse(fs.readFileSync('./data.json'));
+
+var computed = new Map();
+data.forEach(element => {
+    let interestingPart = element.split('@')[1];
+    let startingPos = interestingPart.split(':')[0];
+    let size = interestingPart.split(':')[1];
+    let startingX = parseInt(startingPos.split(',')[0]);
+    let startingY = parseInt(startingPos.split(',')[1]);
+    let stepX = parseInt(size.split('x')[0]);
+    let stepY = parseInt(size.split('x')[1]);
+
+    for (let i = startingX; i < (startingX + stepX); i++) {
+        for (let j = startingY; j < (startingY + stepY); j++) {
+            computed.set(i + '-' + j, computed.get(i + '-' + j) ? computed.get(i + '-' + j) + 1 : 1);
+        }
+    }
+});
+
+let count = 0;
+for (let value of computed.values()) {
+    if (value > 1) {
+        count = count + 1;
+    }
+}
+
+console.log(count);
