1676b4b7daa608762320f73e4d9779532ba09b06
Andre Maxime <maxime.andre@zenika.com>
2018-12-04 18:57:03 +0100

    day4:fix

--- day4/day4_step1.js
+++ day4/day4_step1.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-let data = JSON.parse(fs.readFileSync('./day4/data.json'));
+let data = JSON.parse(fs.readFileSync('./data.json'));
 
 var sortData = (data) => {
     return data.map((value) => {
@@ -29,13 +29,13 @@ var extractGuardsData = (data) => {
             currentMinuteWakesUp = element.date.getMinutes();
             if (!guardsData.get(currentGuardId)) {
                 let minutesAsleep = new Array(60).fill(0);
-                for (let i=currentMinuteAsleep;i < currentMinuteWakesUp;i++) {
+                for (let i = currentMinuteAsleep; i < currentMinuteWakesUp; i++) {
                     minutesAsleep[i] = 1;
                 }
                 guardsData.set(currentGuardId, minutesAsleep);
             } else {
                 let minutesAsleep = guardsData.get(currentGuardId);
-                for (let i=currentMinuteAsleep;i < currentMinuteWakesUp;i++) {
+                for (let i = currentMinuteAsleep; i < currentMinuteWakesUp; i++) {
                     minutesAsleep[i] = minutesAsleep[i] + 1;
                 }
                 guardsData.set(currentGuardId, minutesAsleep);
@@ -53,7 +53,7 @@ var findLazyGuard = (guards) => {
         } else {
             if (laziest[1].reduce((a, b) => a + b, 0) < element[1].reduce((a, b) => a + b, 0)) {
                 laziest = element;
-            }  
+            }
         }
     };
     return laziest;
