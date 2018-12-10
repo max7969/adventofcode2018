c899e1ebe4574c145386065a5bb6358ab2bd5ba7
Andre Maxime <maxime.andre@zenika.com>
2018-12-11 00:55:20 +0100

    day9:step2

--- day9/day9_step1.js
+++ day9/day9_step1.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-let data = JSON.parse(fs.readFileSync('./day9/data.json'));
+let data = JSON.parse(fs.readFileSync('./data.json'));
 
 var readRules = (data) => {
     let regexp = /([0-9]*)\ players\;\ last\ marble\ is\ worth\ ([0-9]*)\ points/;
